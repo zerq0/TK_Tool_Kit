7dbaec0abd228e641b2839a3af01694dc78642a1
zerq0 <kakimzhantair@gmail.com>
2026-05-05 17:50:07 +0500

    basic features added

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import argparse
 from scanner import scan_port
-from ports import parse_ports
-import scanner.py
+# from ports import parse_ports
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host")
+    parser.add_argument("--port", type=int)
+    args = parser.parse_args()
+    scan_port(args.host, args.port)
+if __name__ == "__main__":
+    main()
+
