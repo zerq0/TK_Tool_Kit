dda371ca3c0bec89bbafb483b6044767da02df04
Tair <kakimzhantair@gmail.com>
2026-06-16 11:14:09 +0500

    working on improving a ports parsing system, adding the support of ports ranges

--- ports.py
+++ ports.py
@@ -2,7 +2,18 @@ def parse_ports(ports):
     splitted = ports.strip().split(",")
     ports_list = []
     for x in splitted:
-        if int(x) >= 1 and int(x)<=65535:
-            ports_list.append(int(x))
-    return ports_list
+        if x == "":
+            print("invalid port input")
+            exit
+        if "-" in x:
+            start, end = x.split("-")
+            if end< start:
+                temp = start
+                start = end
+                end = temp
+            if int(start) >= 1 and int(end) <= 65535:
+                ports_list.extend(range(int(start), int(end)+1))
+        else:
 
+    return sorted(ports_list)
+    
