fe869dda96da9373fed1825f5e0c1502df69e204
Tair <kakimzhantair@gmail.com>
2026-06-29 18:45:53 +0500

    added an error message through raise valueerror and fixed some minor issues

--- ports.py
+++ ports.py
@@ -4,16 +4,18 @@ def parse_ports(ports):
     for x in splitted:
         if x == "":
             print("invalid port input")
-            exit
+            raise ValueError
         if "-" in x:
             start, end = x.split("-")
-            if end< start:
+            if int(end) < int(start):
                 temp = start
                 start = end
                 end = temp
             if int(start) >= 1 and int(end) <= 65535:
                 ports_list.extend(range(int(start), int(end)+1))
         else:
+            if int(x) >=1 and int(x) <= 65535:
+                ports_list.append(int(x))
 
     return sorted(ports_list)
     
