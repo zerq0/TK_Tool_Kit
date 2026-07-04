97239ccf21ff23417d43ef471e6dc828a14cd276
Tair <kakimzhantair@gmail.com>
2026-07-04 19:28:45 +0500

    rewrote ports function to make it cleaner and lighter

--- ports.py
+++ ports.py
@@ -2,18 +2,21 @@ def parse_ports(ports):
     splitted = ports.strip().split(",")
     ports_list = set()
     for x in splitted:
-        x=x.strip()
+        x = x.strip()
         if x == "":
             raise ValueError("Invalid port input")
         if "-" in x:
             start, end = x.split("-")
-            if int(end) < int(start):
-                raise ValueError
-            if int(start) >= 1 and int(end) <= 65535 and int (end) >=1 and int (start) <=65535:
-                ports_list.update(range(int(start), int(end)+1))
+            start = int(start)
+            end = int(end)
+            if end < start:
+                raise ValueError ("Port out of range")
+            if start >= 1 and end <= 65535 and end >=1 and start <=65535:
+                ports_list.update(range(start, end+1))
         else:
-            if int(x) >=1 and int(x) <= 65535:
-                ports_list.add(int(x))
+            port = int(x)
+            if port >=1 and port <= 65535:
+                ports_list.add(port)
 
     return sorted(ports_list)
     
